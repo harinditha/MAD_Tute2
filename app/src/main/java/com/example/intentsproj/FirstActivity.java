cda88d6d98cde6da63c254425a9a27c2273ace4d
harinditha <harinditharuchirawya@yahoo.com>
2020-03-24 22:53:14 +0530

    passing values

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -7,39 +7,74 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.view.Menu;
 
 public class FirstActivity extends AppCompatActivity {
-    private Button button;
+//    private Button button;
 //    String myString = "You just clicked the OK button";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        button = (Button) findViewById(R.id.ok);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        openActivity2();
+
+    }
+
+//    public void showtoast() {
+//        Toast toast = Toast.makeText(FirstActivity.this, "You just clicked the OK button", Toast.LENGTH_SHORT);
+//        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+//        toast.show();
+//    }
+
+
+        public void openActivity2() {
+            Button Btn = (Button) findViewById(R.id.ok);
+            Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 //                Toast.makeText(getApplicationContext(),myString,Toast.LENGTH_SHORT).show();
-                showtoast();
-                openActivity2();
-            }
-        })
-        ;
+//                showtoast();
+
+                    EditText userTextEntry = (EditText) findViewById(R.id.editText3);
+                    String userData = userTextEntry.getText().toString();
+                    int userNumber = Integer.parseInt(userData);
+
+                    Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                    intent.putExtra("parameter name", userNumber);
+                    startActivity(intent);
+
+                    showtoast();
+
+//                openActivity2();
+                }
+                public void showtoast() {
+                    Toast toast = Toast.makeText(FirstActivity.this, "You just clicked the OK button", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+                    toast.show();
+                }
+
+            });
+        }
     }
 
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-   }
+//    public void openActivity2() {
+//        Intent intent = new Intent(this, SecondActivity.class);
+//        intent.putExtra("parameter name", "userNumber");
+//        startActivity(intent);
+//   }
 
-    public void showtoast() {
-        Toast toast = Toast.makeText(this, "You just clicked the OK button", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
-        toast.show();
-    }
-
-}
+ //   public void showtoast() {
+//        Toast toast = Toast.makeText(this, "You just clicked the OK button", Toast.LENGTH_SHORT);
+//        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+//        toast.show();
+//    }
+//
+//}
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -2,7 +2,13 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.app.Activity;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +16,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        //Get the Intent that started us to find the parameter (extra).
+        Intent intent = getIntent();
+        int myValue = intent.getIntExtra("Parameter name", 0);
+        //display the value to the screen.
+        TextView displayNumber = (TextView) findViewById(R.id.textView5);
+        displayNumber.setText("" + myValue);
     }
 }
